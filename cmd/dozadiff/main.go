5286ce8c312045c74990b6149440fa58166e1f3e
Magnus Holm <judofyr@gmail.com>
2020-02-17 13:27:44 +0100

    Better error handling in dozadiff

--- cmd/dozadiff/main.go
+++ cmd/dozadiff/main.go
@@ -7,37 +7,53 @@ import (
 	"os"
 )
 
-func readJson(jsonPath string) interface{} {
+func readJson(jsonPath string) (interface{}, error) {
 	jsonFile, err := os.Open(jsonPath)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 	decoder := json.NewDecoder(jsonFile)
 	var doc interface{}
 	err = decoder.Decode(&doc)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
-	return doc
+	return doc, nil
 }
 
-func main() {
-	if len(os.Args) <= 2 {
-		fmt.Printf("usage: dozadiff left.json right.json\n")
-		return
+func run(leftPath, rightPath string) error {
+	aDoc, err := readJson(os.Args[1])
+	if err != nil {
+		return err
 	}
-
-	aDoc := readJson(os.Args[1])
-	bDoc := readJson(os.Args[2])
+	bDoc, err := readJson(os.Args[2])
+	if err != nil {
+		return err
+	}
 
 	patch, err := mendoza.CreatePatch(aDoc, bDoc)
 	if err != nil {
-		panic(err)
+		return err
 	}
 
 	encoder := json.NewEncoder(os.Stdout)
 	err = encoder.Encode(patch)
 	if err != nil {
-		panic(err)
+		return err
+	}
+
+	return nil
+}
+
+func main() {
+	if len(os.Args) != 3 {
+		fmt.Printf("usage: dozadiff left.json right.json\n")
+		return
+	}
+
+	err := run(os.Args[1], os.Args[2])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		os.Exit(1)
 	}
 }
